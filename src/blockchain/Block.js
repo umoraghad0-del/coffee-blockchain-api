21d0292eb39f0166a514ce385cb69e21a95b7b60
Najma Hasan <umoraghad0@gmail.com>
2026-08-19 19:24:48 +0200

    feat: implement proof of work mining

--- src/blockchain/Block.js
+++ src/blockchain/Block.js
@@ -6,11 +6,21 @@ class Block {
         this.timestamp = timestamp;
         this.transactions = transactions;
         this.previousHash = previousHash;
+        this.nonce = 0;
+        this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        const data = `${this.index}${this.timestamp}${JSON.stringify(this.transactions)}${this.previousHash}`;
+        const data = `d${this.index}${this.timestamp}${JSON.stringify(this.transactions)}${this.previousHash}${this.nonce}`;
         return crypto.createHash("sha256").update(data).digest("hex");
     }
+    mineBlock(difficulty) {
+  const target = "0".repeat(difficulty);
+
+  while (!this.hash.startsWith(target)) {
+    this.nonce++;
+    this.hash = this.calculateHash();
+  }
+}
 }
 export default Block;
