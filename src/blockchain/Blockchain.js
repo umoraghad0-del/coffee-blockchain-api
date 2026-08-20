032c1a2d6ae74a5dcde878fc47afa26445a05d02
Najma Hasan <umoraghad0@gmail.com>
2026-08-20 11:33:03 +0200

    feat: implement chain validation

--- src/blockchain/Blockchain.js
+++ src/blockchain/Blockchain.js
@@ -13,6 +13,23 @@ class Blockchain {
 addTransaction(transaction) {
   this.pendingTransactions.push(transaction);
 }
+isChainValid() {
+  for (let i = 1; i < this.chain.length; i++) {
+    const currentBlock = this.chain[i];
+    const previousBlock = this.chain[i - 1];
+
+    if (currentBlock.hash !== currentBlock.calculateHash()) {
+      return false;
+    }
+
+    if (currentBlock.previousHash !== previousBlock.hash) {
+      return false;
+    }
+  }
+
+  return true;
 }
+}
+
 
 export default Blockchain;
