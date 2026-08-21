9dd3e77fa6b889074b577c6b3504ff7144ca4bd2
Najma Hasan <umoraghad0@gmail.com>
2026-08-21 18:52:23 +0200

    feat: implement transaction POST endpoint

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ import express from "express";
 import Blockchain from "./blockchain/Blockchain.js";
 
 const app = express();
+app.use(express.json());
 const blockchain = new Blockchain();
 app.get("/blockchain", (req, res) => {
   res.status(200).json(blockchain);
+});
+app.post("/transactions", (req, res) => {
+  blockchain.addTransaction(req.body);
+
+  res.status(201).json(req.body);
 });
 export default app;
